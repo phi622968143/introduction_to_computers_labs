7a33a8f374d99975ff7e911eb37d36f2721cfb14
F54101056 <40814498+phi622968143@users.noreply.github.com>
2022-10-19 11:08:56 +0800

    Create lab07_plus.py
    我愛柴柴

---
+++ lab07/lab07_plus.py
@@ -0,0 +1,55 @@
+class animal(object):
+    def __init__(self, weight, mood):
+        self.weight = weight  # 定義共用屬性
+        self.mood = mood
+
+    def dogswalk(self, times):
+        self.weight = self.weight-0.2*times
+        self.mood = self.mood+times*2
+
+    def dogsfeed(self, times):
+        self.weight = self.weight+0.2*times
+        self.mood = self.mood+times
+
+    def dogsbath(self, times):
+        self.mood = self.mood-2*times
+
+
+class Dogs(animal):
+    def __init__(self, weight, mood):
+        super().__init__(weight, mood)
+
+
+class Cats(animal):
+    def __init__(self, weight, mood):
+        super().__init__(weight, mood)
+
+    def catswalk(self, times):
+        self.weight = self.weight-0.1*times
+        self.mood = self.mood-times
+
+    def catsfeed(self, times):
+        self.weight = self.weight+0.1*times
+        self.mood = self.mood+times
+
+    def catsbath(self, times):
+        self.mood = self.mood-2*times
+
+
+class Shiba(animal):
+    def __init__(self, weight, mood):
+        super().__init__(weight, mood)
+
+    def shifeed(self, times):
+        self.weight = self.weight+times*0.3
+        self.mood = self.mood+5*times
+
+    def shimood(self):  # self mean inher the class attribute
+        if (self.mood > 100):
+            print("mood最高為100")
+shiba=Shiba(5,70)
+shiba.dogswalk(10)
+shiba.shifeed(20)
+shiba.dogsbath(4)
+print("柴犬現在的體重= "+str(shiba.weight)+"kg, 心情 "+str(shiba.mood))
+shiba.shimood()
