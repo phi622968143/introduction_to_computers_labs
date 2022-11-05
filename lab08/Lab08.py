503ee22fd647fe5d44301395701e15f0ff8c7cf5
F54101056 <40814498+phi622968143@users.noreply.github.com>
2022-11-05 12:04:59 +0800

    Update Lab08.py

--- lab08/Lab08.py
+++ lab08/Lab08.py
@@ -1,6 +1,6 @@
 import os
 
-dir=list(os.getwd())
+dir=list(os.getcwd())
 dirs=list(os.listdir())
 print(dir)
 print(dirs)
@@ -11,6 +11,7 @@ for listitem in dir:
         f.write('%s' % listitem)
     else:
         f.write("\n")#os.linesep 間距太大 我不喜歡
+f.write(os.linesep)#wow os.linesep 在這有用 "\n" 爛掉了
 for listitem in dirs:
     f.write('%s\n' % listitem)
 f.close()
