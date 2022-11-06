7546d464109029ab4ef5c0668132db5a9169f8da
F54101056 <40814498+phi622968143@users.noreply.github.com>
2022-11-06 14:35:03 +0800

    Update Lab08.py

--- lab08/Lab08.py
+++ lab08/Lab08.py
@@ -1,17 +1,15 @@
 import os
 
-dir=list(os.getcwd())
+dir=list(os.getcwd().split(os.path.sep))
 dirs=list(os.listdir())
 print(dir)
 print(dirs)
 path = 'f54101056.txt'
 f = open(path, 'w')
 for listitem in dir:
-    if(listitem!=os.sep):
-        f.write('%s' % listitem)
-    else:
-        f.write("\n")#os.linesep 間距太大 我不喜歡
+    f.write('%s\n' % listitem)
 f.write(os.linesep)#wow os.linesep 在這有用 "\n" 爛掉了
 for listitem in dirs:
     f.write('%s\n' % listitem)
 f.close()
+
